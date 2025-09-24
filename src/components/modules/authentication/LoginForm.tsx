26f5faac427e90c44ca1af6ce4a409a8967024f2
Md Habibullah <mdhabibullah.work@gmail.com>
2025-09-24 16:46:34 +0600

    login and logout form ready

--- src/components/modules/authentication/LoginForm.tsx
+++ src/components/modules/authentication/LoginForm.tsx
@@ -1,7 +1,163 @@
-export default function LoginForm() {
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import config from "@/config";
+import { cn } from "@/lib/utils";
+import { useLoginMutation } from "@/redux/features/auth/auth.api";
+import { type FieldValues, type SubmitHandler, useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router";
+import { toast } from "sonner";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+
+export function LoginForm({
+    className,
+    ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+    const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
+    const form = useForm({
+        //! For development only
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+    const [login] = useLoginMutation();
+
+    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+        try {
+            const res = await login(data).unwrap();
+            if (res.success) {
+                toast.success("Logged in successfully");
+                navigate("/");
+            }
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (err: any) {
+            console.error(err?.data?.message);
+
+            // Handle errors inline in form
+            if (err?.data?.message === "User does not exist") {
+                form.setError("email", { type: "manual", message: "Email does not exist" });
+            }
+            else if (err?.data?.message === "User is blocked or deleted") {
+                form.setError("email", { type: "manual", message: "User is blocked or deleted" });
+            }
+            else if (err?.data?.message === "Password does not matched") {
+                form.setError("password", { type: "manual", message: "Password is incorrect" });
+            }
+            // else if (err?.data?.message === "User is not verified") {
+            //     toast.error("Your account is not verified");
+            //     navigate("/verify", { state: data.email });
+            // }
+            else {
+                toast.error("Something went wrong. Please try again");
+            }
+        }
+    };
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
-        <div>
-            <h1> This is LoginForm component </h1>
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="text-2xl font-bold">Login to your account</h1>
+                <p className="text-balance text-sm text-muted-foreground">
+                    Enter your email below to login to your account
+                </p>
+            </div>
+            <div className="grid gap-6">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="Enter your email"
+                                            {...field}
+                                            value={field.value || ""}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <div className="relative">
+                                            <Input
+                                                type={showPassword ? "text" : "password"}
+                                                placeholder="********"
+                                                {...field}
+                                                value={field.value || ""}
+                                                className="pr-10"
+                                            />
+                                            <button
+                                                type="button"
+                                                onClick={togglePasswordVisibility}
+                                                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
+                                                tabIndex={-1} // Prevent tab focus on the button
+                                            >
+                                                {showPassword ? (
+                                                    <EyeOff className="h-4 w-4" />
+                                                ) : (
+                                                    <Eye className="h-4 w-4" />
+                                                )}
+                                            </button>
+                                        </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
+                    </form>
+                </Form>
+
+                <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                    <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                        Or continue with
+                    </span>
+                </div>
+
+                {/*//* http://localhost:5000/api/v1/auth/google */}
+                <Button
+                    onClick={() => window.open(`${config.baseUrl}/auth/google`)}
+                    type="button"
+                    variant="outline"
+                    className="w-full cursor-pointer"
+                >
+                    Login with Google
+                </Button>
+            </div>
+            <div className="text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <Link to="/register" replace className="underline underline-offset-4">
+                    Register
+                </Link>
+            </div>
         </div>
     );
 }
--- src/components/modules/authentication/RegisterForm.tsx
+++ src/components/modules/authentication/RegisterForm.tsx
@@ -1,7 +1,224 @@
-export default function RegisterForm() {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { cn } from "@/lib/utils";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Password from "@/components/ui/Password";
+import { useRegisterMutation } from "@/redux/features/auth/auth.api";
+import { toast } from "sonner";
+import config from "@/config";
+
+const registerSchema = z
+    .object({
+        name: z
+            .string()
+            .min(3, { message: "Name is too short" })
+            .max(50, { message: "Name is too long" }),
+        email: z.string().email({ message: "Invalid email address" }),
+        password: z
+            .string()
+            .min(8, { message: "Password must be at least 8 characters long" })
+            .regex(/^(?=.*[A-Z])/, "Password must contain at least 1 uppercase letter")
+            .regex(/^(?=.*[!@#$%^&*])/, "Password must contain at least 1 special character")
+            .regex(/^(?=.*\d)/, "Password must contain at least 1 number"),
+        confirmPassword: z.string().min(8, { message: "Confirm Password is too short" }),
+        role: z
+            .string()
+            .refine((val) => val !== "" && val !== undefined, { message: "Please select a role" })
+            .refine((val) => ["SENDER", "RECEIVER"].includes(val), { message: "Please select a valid role" }),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+    });
+
+export function RegisterForm({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+    const [register] = useRegisterMutation();
+    const navigate = useNavigate();
+
+    const form = useForm<z.infer<typeof registerSchema>>({
+        resolver: zodResolver(registerSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+            role: "",
+        },
+    });
+
+    const onSubmit = async (data: z.infer<typeof registerSchema>) => {
+        const userInfo = {
+            name: data.name,
+            email: data.email,
+            password: data.password,
+            role: data.role,
+        };
+
+        try {
+            const res = await register(userInfo).unwrap();
+            if (res.success) {
+                toast.success("User created successfully");
+                navigate("/login");
+            }
+        } catch (error: any) {
+            // Handle Zod validation errors from backend
+            if (error?.data?.errors && Array.isArray(error.data.errors)) {
+                error.data.errors.forEach((err: { field: string; message: string }) => {
+                    form.setError(err.field as keyof typeof data, { message: err.message });
+                });
+            } else if (error?.data?.message) {
+                toast.error(error.data.message);
+            } else {
+                toast.error("Registration failed. Please try again.");
+            }
+        }
+    };
+
     return (
-        <div>
-            <h1> This is RegisterForm component </h1>
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="text-2xl font-bold">Register your account</h1>
+                <p className="text-sm text-muted-foreground">
+                    Enter your details to create an account
+                </p>
+            </div>
+
+            <div className="grid gap-6">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Your Name" {...field} />
+                                    </FormControl>
+                                    <FormDescription className="sr-only">
+                                        This is your public display name.
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Your Email Address" type="email" {...field} />
+                                    </FormControl>
+                                    <FormDescription className="sr-only" />
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="role"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Role</FormLabel>
+                                    <Select onValueChange={field.onChange} value={field.value} defaultValue="">
+                                        <FormControl className="w-full">
+                                            <SelectTrigger>
+                                                <SelectValue placeholder="Select your role" />
+                                            </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                            <SelectItem value="SENDER">Sender</SelectItem>
+                                            <SelectItem value="RECEIVER">Receiver</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                    <FormDescription className="text-muted-foreground px-2 text-[12px]">
+                                        Select whether you will be sending or receiving parcels
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Password {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="confirmPassword"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Confirm Password</FormLabel>
+                                    <FormControl>
+                                        <Password {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <Button type="submit" className="w-full">
+                            Submit
+                        </Button>
+                    </form>
+                </Form>
+
+                <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                    <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                        Or continue with
+                    </span>
+                </div>
+
+                <Button
+                    onClick={() => window.open(`${config.baseUrl}/auth/google`)}
+                    type="button"
+                    variant="outline"
+                    className="w-full cursor-pointer"
+                >
+                    Login with Google
+                </Button>
+            </div>
+
+            <div className="text-center text-sm">
+                Already have an account?{" "}
+                <Link to="/login" className="underline underline-offset-4">
+                    Login
+                </Link>
+            </div>
         </div>
     );
 }
